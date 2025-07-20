590396cbc6df76f59fa3d657abc9c3aa12cf5b8a
chauhan-scion <chauhanscion@gmail.com>
2025-07-20 14:30:02 +0530

    another change

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import cors from 'cors';
 import {User} from './models/User.js'; // Assuming you have a User model defined in models/User
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
